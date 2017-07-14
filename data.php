d1051d9debc77e57230e8e1072cd0bf1eacbf9ad
Pragati Vyas <pvyas@m-C02NC2EXG3QP.local>
2017-07-14 15:40:41 -0700

    add files

---
+++ data.php
@@ -0,0 +1,5 @@
+<?php
+// prints something like: Wednesday the 15th
+//echo date('l \t\h\e jS');
+echo $_GET['name'];
+?>
---
+++ date.php
@@ -0,0 +1,5 @@
+<?php
+// prints something like: Wednesday the 15th
+echo date('l js \of F Y h:i:s A');
+//echo $_GET['name'];
+?>
