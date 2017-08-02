dbba6c240a212d9910b713be40809470486025d5
Pragati Vyas <pragativyas@Pragatis-MacBook-Air.local>
2017-08-02 07:51:01 -0700

    Node callbacksPromises

---
+++ demos/Node/Callbacks_Promises/node-5-3-local-modules/weather.js
@@ -0,0 +1,32 @@
+var request = require('request');
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&units=imperial';
+
+module.exports = function (callback) {
+	request({
+		url: url,
+		json: true
+	}, function (error, response, body) {
+		if (error) {
+			callback('Unable to fetch weather.');
+		} else {
+			// console.log(JSON.stringify(body, null, 4));
+			// It's 77.77 in Philadelphia!
+			callback('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+		}
+	});
+}
+
+/*
+request({
+	url: url,
+	json: true
+}, function (error, response, body) {
+	if (error) {
+		console.log('Unable to fetch weather.');
+	} else {
+		// console.log(JSON.stringify(body, null, 4));
+		// It's 77.77 in Philadelphia!
+		console.log('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+	}
+});
+*/
